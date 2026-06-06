58102b5425f08b61bfe1ba36a8633cca2bcacf20
ValenceAgborchi <vagborchi27@gmail.com>
2026-06-06 16:10:23 -0400

    Add: anagram-groups - submission-69

---
+++ Data Structures & Algorithms/anagram-groups/submission-69.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = defaultdict(list)
+        for i in strs:
+            sorteds = sorted(i)
+            output[tuple(sorteds)].append(i)
+        
+        return list(output.values())
